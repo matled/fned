fb4bf766d2a085253d1a2b60b08b7b779c713169
Matthias Lederhofer <matled@gmx.net>
2012-01-17 23:01:07 +0100

    add copyright/license to source files

--- lib/fned/filename_edit.rb
+++ lib/fned/filename_edit.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2012 Matthias Lederhofer <matled@gmx.net>
+#
+# This file is part of fned.
+#
+# fned is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# fned is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fned.  If not, see <http://www.gnu.org/licenses/>.
 require 'pathname'
 require 'fned/edit_list'
 require 'fned/rename'
